95583d07d6c105c169a09d4cc5f2632fa4de4b1c
euryalis <euryalis@hotmail.co.jp>
2018-10-05 18:43:55 +0900

    install zap for logging

---
+++ zap/zap.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type user struct {
+	Name      string
+	Email     string
+	CreatedAt time.Time
+}
+
+func (u user) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddString("name", u.Name)
+	enc.AddString("email", u.Email)
+	enc.AddInt64("created_at", u.CreatedAt.UnixNano())
+	return nil
+}
+
+func main() {
+	logger, _ := zap.NewDevelopment()
+	user := &user{
+		Name: "Zap",
+		Email: "zap@sample.com",
+		CreatedAt: time.Now(),
+	}
+	logger.Info("object sample", zap.Object("userObj", user))
+}
